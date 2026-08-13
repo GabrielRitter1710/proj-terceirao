4db2773fe693dbb50503900ab607bd5981de00b2
Gabriel Dias Ritter <bielritter@gmail.com>
2026-08-13 10:07:48 -0300

    13082026

---
+++ script.js
@@ -0,0 +1,22 @@
+/* ============================================================
+   JORNAL DO TERCEIRÃO EDUCOOPE
+   JavaScript
+   ============================================================ */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Jornal do Terceirão Educoope carregado!");
+
+    /*
+     * Futuramente, podemos adicionar aqui:
+     *
+     * - Menu mobile
+     * - Filtro por categorias
+     * - Carrossel de fotos
+     * - Busca por notícias
+     * - Carregamento de matérias
+     * - Animações ao entrar na tela
+     * - Contador regressivo para a formatura
+     */
+
+});
